887a6161381d9c67ce8fa24526be19743a105163
FaridBAHR1 <f4ridb4hri@gmail.com>
2023-05-22 12:35:27 +1100

    View - Template Tag

--- js/site.js
+++ js/site.js
@@ -52,18 +52,30 @@ function Free5(freeValue, fiveValue){
 
 //display bold numbers - view/display
 function displayData(f5Array){
-    let templateRows = "";
+    //get table body element from page/document
+    let templateBody = document.getElementById("results");
+
+    //get template row
+    let templateRow = document.getElementById("f5Template");
+
+    //clear table first
+    tableBody.innerHTML = "";
 
     //loop over Array & create tablerow for each item
-    for (let index = 0; index < f5Array.length; index++) {
-        let className = "even";
-        let number = numbers[index]; //assignemnt check
-        if(number % 2 == 0) //equality check
-        {
-            className = "even";}
-        else {className = "odd";}
-        templateRows += `<tr><td class="${className}" >${number}</td></tr>`; //creating HTML Table
-    };
+    for (let index = 0; index < f5Array.length; index += 5) {
+        //invoke table row from app.html
+        let tableRow = document.importNode(templateRow.content, true);
+        //determine template width
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].textContent = fbData[i];
+        rowCols[1].textContent = fbData[i+1];
+        rowCols[2].textContent = fbData[i+2];
+        rowCols[3].textContent = fbData[i+3];
+        rowCols[4].textContent = fbData[i+4];
+
+        //append value in table row
+        tableBody.appendChild(tableRow);
+      };
 
     //Add all rows to table
     document.getElementById("results").innerHTML = templateRows;
